0b784576968d6e0f0b23a21919536951992c0b64
wip-abramson <will@truu.id>
2019-04-09 17:55:24 +0100

    initial poc code

---
+++ src/lib/components/EcosystemWrapper.js
@@ -0,0 +1,15 @@
+/**
+ * Created by will on 09/04/19.
+ */
+import React from 'react';
+
+const EcosystemWrapper = ({children}) => {
+  return (
+    <div>
+      <h1>Ecosystem Wrapper Header</h1>
+      {children}
+    </div>
+  )
+};
+
+export default EcosystemWrapper;
---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+import TextInput from "./TextInput";
+import EcosystemWrapper from "./components/EcosystemWrapper";
+import "./assets/styles/main.scss"
+export { TextInput };
+export { EcosystemWrapper }
